680468cb2f8f2aa14b83707e80fb30bc331af601
Berkay Çiftçi <55693589+berkay-c@users.noreply.github.com>
2020-11-09 22:22:33 +0300

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,29 @@
+//190202056 BErkay yasin ciftci
+package nyplab.Lab1calisma1;
+import java.util.Scanner;
+public class Lab1Calisma1 {
+
+    public static void main(String[] args) {
+       //çalýþma 1
+        int toplam = 1;
+        for (int i = 1; i <=10; i++) {
+            toplam = toplam * i;
+
+        }
+        System.out.println("10!=" + toplam);
+        //çalýþma 2
+        /*
+         System.out.println("3 adet kelime girin:");
+        Scanner girdi=new Scanner(System.in);
+        String kelime1=girdi.next();
+        String kelime2=girdi.next();
+        String kelime3=girdi.next();
+        System.out.print(kelime1+"\\"+ kelime2 + "\\" + kelime3);
+        */
+    }
+
+}
+
+
+
+
