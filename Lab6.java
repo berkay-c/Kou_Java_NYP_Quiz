895e2274dd3c6bf502b03f7f725f5d6512179b87
Berkay Çiftçi <55693589+berkay-c@users.noreply.github.com>
2020-11-17 13:21:16 +0300

    Lab6.java

---
+++ Lab6.java
@@ -0,0 +1,73 @@
+package nyplab.Lab6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainClass {
+//    // Calisma 1 
+//    public static void main(String[] args) {
+//        int[] ilkdizi = new int[4];
+//        System.out.print("Dizinin ilk hali: ");
+//        for (int i = 0; i < 4; i++) {
+//            ilkdizi[i] = (int) (Math.random() * 9);
+//            System.out.print(ilkdizi[i] + "\t");
+//
+//        }
+//        yenidizi(ilkdizi);
+//        System.out.println("\n Dizinin son hali:");
+//        for (int i = 0; i < ilkdizi.length; i++) {
+//            System.out.print(ilkdizi[i] + "\t");
+//
+//        }
+//
+//    }
+//
+//    public static int[] yenidizi(int[] dizi) {
+//
+//        for (int i = 0; i < dizi.length; i++) {
+//            dizi[i] = dizi[i] * 2;
+//
+//        }
+//        return dizi;
+//    }
+    // Calisma 2 
+
+//    public static void main(String[] args) {
+//        Scanner girdi = new Scanner(System.in);
+//        ArrayList<Integer> list = new ArrayList<Integer>();
+//        while (true) {
+//            System.out.println("Lütfen bir sayı giriniz. Eğer çıkmak istiyorsanız 0’a basınız. ");
+//            int x = girdi.nextInt();
+//            if (x == 0) {
+//                break;
+//            } else {
+//                list.add(x);
+//
+//            }
+//        }
+//        System.out.println("Kullanıcının girdiği değerler:");
+//        girilenElemanlar(list);
+//        int[] dizi = new int[list.size()];
+//        for (int i = 0; i < list.size(); i = i + 2) {
+//            dizi[i] = list.get(i + 1);
+//            dizi[i + 1] = list.get(i);
+//
+//        }
+//
+//        System.out.println("\nModifiye edilmiş değerler:");
+//        for (int i = 0; i < list.size(); i++) {
+//            list.set(i, dizi[i]);
+//
+//        }
+//        girilenElemanlar(list);
+//    }
+//
+//    public static void girilenElemanlar(ArrayList<Integer> list) {
+//
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.print(list.get(i) + "\t");
+//
+//        }
+//    }
+//
+}
